9e800e2348bbcda5a78d4b784fafd01ad0d52908
Spencer Hirata <hirataspencemama@gmail.com>
2017-08-09 19:48:12 -0700

    :)

--- pixelArt.js
+++ pixelArt.js
@@ -1,9 +1,9 @@
 let box = document.createElement("div");
-box.setAttribute("style", "border:3px solid #000000; width:448px; height:455px;");
+box.setAttribute("style", "border:3px solid #000000; width:442px; height:457px; line-height: 0%;");
 document.body.appendChild(box);
 let divs = document.createElement("div");
-divs.setAttribute("style", "margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; display:inline-block; border:2px solid #000000;position:relative; width:10px; height:10px;");
-let amtPixels = 800;
+divs.setAttribute("style", "margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; display:inline-block; border:.1px solid black; width:10px; height:10px;");
+let amtPixels = 1444;
 let currentColor = "white";
 let colorList = ["white","blue","red","yellow","green","purple","pink","orange","brown","gray","black"]
 
@@ -32,7 +32,7 @@ document.body.appendChild(CC);
 
 function turnColor(id){
     let xd = document.getElementById(id);
-    xd.setAttribute("style", "background-color:" + currentColor + "; display:inline-block; border:2px solid #000000; width:10px; height:10px;");
+    xd.setAttribute("style", "background-color:" + currentColor + "; display:inline-block; border:.1px solid black;; width:10px; height:10px;");
 }
 
 function setColor(id = "white"){
